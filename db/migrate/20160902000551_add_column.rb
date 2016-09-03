ead645040b35a8dc0b077741ded3b543ed51e92b
michaeljstevens <michaeljstevens@zoho.com>
2016-09-02 21:03:57 -0700

    attempted to fix db

--- db/migrate/20160902000551_add_column.rb
+++
@@ -1,8 +0,0 @@
-class AddColumn < ActiveRecord::Migration
-  def change
-    remove_column :routes, :start_pos
-    remove_column :routes, :end_pos
-    add_column :routes, :start_pos, :string, null: false
-    add_column :routes, :end_pos, :string, null: false
-  end
-end
