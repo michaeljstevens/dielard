161ed37515f1944df87bfb9ad2da5e4c0ddca3fb
michaeljstevens <michaeljstevens@zoho.com>
2016-09-06 12:09:39 -0700

    refactored jbuilder views, started styling workout index

--- frontend/components/workouts/travel_workout_index_item.jsx
+++ frontend/components/workouts/travel_workout_index_item.jsx
@@ -1,24 +1,66 @@
 import React from 'react';
-import RouteIndexItem from '../routes/route_index_item.jsx';
 
 class TravelWorkoutIndexItem extends React.Component {
   constructor(props) {
     super(props);
+    this.setDirections = this.setDirections.bind(this);
+    this.calculateAndDisplayRoute = this.calculateAndDisplayRoute.bind(this);
+    this.travelWorkout = this.props.travelWorkout;
+  }
+
+  componentDidMount() {
+    const mapDOMNode = this.refs.map;
+    const mapOptions = {
+      center: {lat: 37.7758, lng: -122.435},
+      zoom: 13
+    };
+    this.map = new google.maps.Map(mapDOMNode, mapOptions);
+    this.setDirections();
+  }
+
+  setDirections() {
+
+    let rendererOptions = {
+      map: this.map,
+      suppressMarkers: false,
+    };
+
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+
+    this.directionsDisplay.setMap(this.map);
+    this.calculateAndDisplayRoute(this.directionsService, this.directionsDisplay);
+  }
+
+  calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    let that = this;
+    let origin = {lat: this.travelWorkout.route.start_lat, lng: this.travelWorkout.route.start_lng};
+    let destination = {lat: this.travelWorkout.route.end_lat, lng: this.travelWorkout.route.end_lng};
+    directionsService.route({
+      origin: origin,
+      destination: destination,
+      travelMode: 'WALKING'
+    }, function(response, status) {
+      if(status === 'OK') {
+        directionsDisplay.setDirections(response);
+      } else {
+        setTimeout( () => {
+          that.calculateAndDisplayRoute(directionsService, directionsDisplay);
+        }, 200);
+      }
+    });
   }
 
   render() {
-    const travelWorkout = this.props.travelWorkout;
 
     return(
       <div className="travelWorkout-index-item">
         <ul className="travel-workout-index-list">
-          <li>{travelWorkout.date}</li>
-          <li>{travelWorkout.notes}</li>
-          <li>{travelWorkout.calories}</li>
+          <li>{this.travelWorkout.date}</li>
+          <li>{this.travelWorkout.notes}</li>
+          <li>{this.travelWorkout.calories}</li>
         </ul>
-        <div className="workout-index-route">
-          <RouteIndexItem route={travelWorkout.route} />
-        </div>
+        <div className="feed-index-map" ref="map"></div>
       </div>
     );
   }
