6baa015d3867dbe44b600c4b6c36c9f8b3dec828
michaeljstevens <michaeljstevens@zoho.com>
2016-09-06 23:03:40 -0700

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -242,3 +242,394 @@ Exercise.create(
   description: "Swing a kettlebell from between your legs until your arms are parallel to the ground for a fixed period of time",
   muscle_group: "Other"
 )
+
+Exercise.create(
+  user_id: 1,
+  title: "Leg Press",
+  description: "Move a loaded sled with your legs from below parallel to lockout.",
+  muscle_group: "Legs"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "Leg Curl",
+  description: "Use your hamstrings to curl a weighted machine focusing on a full contraction.",
+  muscle_group: "Legs"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "Leg Extensions",
+  description: "Extend your legs against a weighted machine being careful not to strain your knees.",
+  muscle_group: "Legs"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "Dumbbell Bench Press",
+  description: "Move dumbbells from your chest to a locked out position above your body while laying on a bench.",
+  muscle_group: "Chest"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "T-Bar Row",
+  description: "Keeping a straight back, move one end of a barbell from the floor to your chest without using momentum.",
+  muscle_group: "Back"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "Side Lateral Raise",
+  description: "Move your arms out to the side while holding dumbbells.",
+  muscle_group: "Shoulders"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "Sit Up",
+  description: "Move from a laying to seated position while keeping a straight back.",
+  muscle_group: "Core"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "Hip Adduction",
+  description: "Squeeze legs together against weighted machine.",
+  muscle_group: "Glutes"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "Hip Abduction",
+  description: "Push legs apart against a weighted machine.",
+  muscle_group: "Glutes"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "Pull Up",
+  description: "From a hanging position, pull up until chin is above bar without using momentum and with a double overhand grip.",
+  muscle_group: "Back"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "Rear Delt Fly",
+  description: "Move your arms backwards with dumbbells while bent over at the waist without using momentum.",
+  muscle_group: "Shoulders"
+  )
+Exercise.create(
+  user_id: 1,
+  title: "Machine Fly",
+  description: "Close hands together against weighted machine keeping your scapula retracted.",
+  muscle_group: "Chest"
+  )
+
+  TravelWorkout.create(
+    user_id: 1,
+    route_id: 1,
+    date: "Mon, 01 Aug 2016",
+    calories: 406,
+    notes: "Walked this route during rush hour. Would not recommend!"
+  )
+  TravelWorkout.create(
+    user_id: 1,
+    route_id: 3,
+    date: "Tue, 02 Aug 2016",
+    calories: 302,
+    notes: "Got a cramp halfway through. I need to remember to eat bananas. "
+  )
+  TravelWorkout.create(
+    user_id: 1,
+    route_id: 2,
+    date: "Thu, 04 Aug 2016",
+    calories: 454,
+    notes: "This was too easy and a bit dangerous."
+  )
+  TravelWorkout.create(
+    user_id: 1,
+    route_id: 10,
+    date: "Sat, 06 Aug 2016",
+    calories: 140,
+    notes: "Loved walking through Napa today. Glad I brought a raincoat."
+  )
+  TravelWorkout.create(
+    user_id: 1,
+    route_id: 6,
+    date: "Tue, 06 Sep 2016",
+    calories: 590,
+    notes: "This was a tough but rewarding ride."
+  )
+  TravelWorkout.create(
+    user_id: 1,
+    route_id: 11,
+    date: "Thu, 18 Aug 2016",
+    calories: 491,
+    notes: "Just realized this is nearly a 10k and it was honestly pretty easy."
+  )
+  TravelWorkout.create(
+    user_id: 1,
+    route_id: 5,
+    date: "Thu, 13 Oct 2016",
+    calories: 287,
+    notes: "Solid 5k run. Got a little winded by the end but no big deal."
+  )
+  TravelWorkout.create(
+    user_id: 1,
+    route_id: 7,
+    date: "Thu, 18 Aug 2016",
+    calories: 647,
+    notes: "I forgot water again! Ugh..."
+  )
+  TravelWorkout.create(
+    user_id: 1,
+    route_id: 6,
+    date: "Mon, 29 Aug 2016",
+    calories: 590,
+    notes: "Great until I got downtown, then way too much traffic."
+  )
+
+  StaticWorkout.create(
+   user_id: 1,
+   date: "Tue, 06 Sep 2016",
+   duration: 60,
+   focus: "",
+   calories: 468,
+   notes: "Had some rotator cuff pain while benching. Need to focus on keeping my shoulders tucked back."
+  )
+  StaticWorkout.create(
+   user_id: 1,
+   date: "Fri, 05 Aug 2016",
+   duration: 90,
+   focus: "",
+   calories: 702,
+   notes: "Liked this long workout but next time I should eat carbs beforehand."
+  )
+  StaticWorkout.create(
+   user_id: 1,
+   date: "Sun, 07 Aug 2016",
+   duration: 75,
+   focus: "",
+   calories: 585,
+   notes: "Gym was super busy for a Sunday!"
+  )
+  StaticWorkout.create(
+   user_id: 1,
+   date: "Tue, 06 Sep 2016",
+   duration: 90,
+   focus: "",
+   calories: 702,
+   notes: "Solid upper body day."
+  )
+  StaticWorkout.create(
+   user_id: 1,
+   date: "Wed, 03 Aug 2016",
+   duration: 75,
+   focus: "",
+   calories: 585,
+   notes: "Hit a squat pr. Felt good on this day."
+  )
+
+  StaticWorkout.create(
+   user_id: 1,
+   date: "Sat, 20 Aug 2016",
+   duration: 120,
+   focus: "",
+   calories: 936,
+   notes: "Longest workout of my life."
+  )
+
+  WorkoutExercise.create(
+    static_workout_id: 1,
+    exercise_id: 1,
+    sets: 5,
+    reps: 5,
+    weight: 185
+  )
+  WorkoutExercise.create(
+    static_workout_id: 1,
+    exercise_id: 3,
+    sets: 3,
+    reps: 5,
+    weight: 260
+  )
+  WorkoutExercise.create(
+    static_workout_id: 1,
+    exercise_id: 8,
+    sets: 3,
+    reps: 10,
+    weight: 225
+  )
+  WorkoutExercise.create(
+    static_workout_id: 2,
+    exercise_id: 2,
+    sets: 5,
+    reps: 5,
+    weight: 90
+  )
+  WorkoutExercise.create(
+    static_workout_id: 2,
+    exercise_id: 6,
+    sets: 5,
+    reps: 5,
+    weight: 65
+  )
+  WorkoutExercise.create(
+    static_workout_id: 2,
+    exercise_id: 4,
+    sets: 3,
+    reps: 10,
+    weight: 50
+  )
+  WorkoutExercise.create(
+    static_workout_id: 2,
+    exercise_id: 5,
+    sets: 3,
+    reps: 10,
+    weight: 45
+  )
+  WorkoutExercise.create(
+    static_workout_id: 2,
+    exercise_id: 7,
+    sets: 3,
+    reps: 60,
+    weight: 1
+  )
+  WorkoutExercise.create(
+    static_workout_id: 3,
+    exercise_id: 9,
+    sets: 3,
+    reps: 50,
+    weight: 35
+  )
+  WorkoutExercise.create(
+    static_workout_id: 3,
+    exercise_id: 3,
+    sets: 5,
+    reps: 5,
+    weight: 185
+  )
+  WorkoutExercise.create(
+    static_workout_id: 3,
+    exercise_id: 1,
+    sets: 3,
+    reps: 10,
+    weight: 95
+  )
+  WorkoutExercise.create(
+    static_workout_id: 4,
+    exercise_id: 3,
+    sets: 3,
+    reps: 5,
+    weight: 315
+  )
+  WorkoutExercise.create(
+    static_workout_id: 4,
+    exercise_id: 14,
+    sets: 5,
+    reps: 5,
+    weight: 180
+  )
+  WorkoutExercise.create(
+    static_workout_id: 4,
+    exercise_id: 19,
+    sets: 5,
+    reps: 5,
+    weight: 1
+  )
+  WorkoutExercise.create(
+    static_workout_id: 4,
+    exercise_id: 8,
+    sets: 3,
+    reps: 10,
+    weight: 185
+  )
+  WorkoutExercise.create(
+    static_workout_id: 4,
+    exercise_id: 12,
+    sets: 3,
+    reps: 15,
+    weight: 90
+  )
+  WorkoutExercise.create(
+    static_workout_id: 5,
+    exercise_id: 2,
+    sets: 5,
+    reps: 5,
+    weight: 135
+  )
+  WorkoutExercise.create(
+    static_workout_id: 5,
+    exercise_id: 21,
+    sets: 3,
+    reps: 20,
+    weight: 50
+  )
+  WorkoutExercise.create(
+    static_workout_id: 5,
+    exercise_id: 13,
+    sets: 3,
+    reps: 10,
+    weight: 45
+  )
+  WorkoutExercise.create(
+    static_workout_id: 5,
+    exercise_id: 6,
+    sets: 5,
+    reps: 5,
+    weight: 70
+  )
+  WorkoutExercise.create(
+    static_workout_id: 5,
+    exercise_id: 15,
+    sets: 3,
+    reps: 15,
+    weight: 30
+  )
+  WorkoutExercise.create(
+    static_workout_id: 5,
+    exercise_id: 5,
+    sets: 3,
+    reps: 10,
+    weight: 50
+  )
+  WorkoutExercise.create(
+    static_workout_id: 6,
+    exercise_id: 1,
+    sets: 5,
+    reps: 5,
+    weight: 150
+  )
+  WorkoutExercise.create(
+    static_workout_id: 6,
+    exercise_id: 10,
+    sets: 3,
+    reps: 15,
+    weight: 350
+  )
+  WorkoutExercise.create(
+    static_workout_id: 6,
+    exercise_id: 11,
+    sets: 3,
+    reps: 10,
+    weight: 45
+  )
+  WorkoutExercise.create(
+    static_workout_id: 6,
+    exercise_id: 12,
+    sets: 3,
+    reps: 10,
+    weight: 100
+  )
+  WorkoutExercise.create(
+    static_workout_id: 6,
+    exercise_id: 17,
+    sets: 3,
+    reps: 15,
+    weight: 40
+  )
+  WorkoutExercise.create(
+    static_workout_id: 6,
+    exercise_id: 18,
+    sets: 3,
+    reps: 15,
+    weight: 60
+  )
+  WorkoutExercise.create(
+    static_workout_id: 6,
+    exercise_id: 3,
+    sets: 1,
+    reps: 7,
+    weight: 325
+  )
